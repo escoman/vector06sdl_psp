27329fbac1491f9d761839265529004cf8401a14
Sarmin Alexey <escoman123@gmail.com>
2026-08-10 13:26:53 +0400

    Условная компиляция дебага

--- psp/src/debuglog.h
+++ psp/src/debuglog.h
@@ -1,9 +1,19 @@
 #pragma once
 
-#include <string>
+#ifndef DEBUG_ENABLED
+#define DEBUG_ENABLED 0
+#endif
 
-/* File-based debug logger for PSP.
- * Writes to ms0:/PSP/GAME/VECTOR06C/debug.log */
+#if DEBUG_ENABLED
+
 void dbglog_open();
 void dbglog(const char *fmt, ...);
 void dbglog_close();
+
+#else
+
+#define dbglog_open()  ((void)0)
+#define dbglog(...)    ((void)0)
+#define dbglog_close() ((void)0)
+
+#endif
